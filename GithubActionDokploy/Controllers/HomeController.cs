714ec7b55f71e587763661bb485c66de7a6d730e
ALIBI Ghazi <123127137+GhaziAlibi@users.noreply.github.com>
2025-02-17 02:43:40 +0100

    test

--- GithubActionDokploy/Controllers/HomeController.cs
+++ GithubActionDokploy/Controllers/HomeController.cs
@@ -1,32 +1,30 @@
 using System.Diagnostics;
 using GithubActionDokploy.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace GithubActionDokploy.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController(ILogger<HomeController> logger, IConfiguration configuration) : Controller
     {
-      _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger = logger;
+        private readonly IConfiguration _configuration = configuration;
 
-    public IActionResult Index()
-    {
-      return View();
-    }
+        public IActionResult Index()
+        {
+            ViewData["Version"] = _configuration["Settings:Version"];
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-  }
 }
